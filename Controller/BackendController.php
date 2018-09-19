cdefe7cf629a8b3532282b196e58ceae2575275d
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-09-19 21:34:04 +0200

    Split controllers per application

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -12,7 +12,7 @@
  */
 declare(strict_types=1);
 
-namespace Modules\Exchange;
+namespace Modules\Exchange\Controller;
 
 use Modules\Navigation\Models\Navigation;
 use Modules\Navigation\Views\NavigationView;
@@ -38,57 +38,8 @@ use Modules\Exchange\Models\PermissionState;
  * @link       http://website.orange-management.de
  * @since      1.0.0
  */
-final class Controller extends ModuleAbstract implements WebInterface
+class BackendController extends Controller
 {
-
-    /**
-     * Module path.
-     *
-     * @var string
-     * @since 1.0.0
-     */
-    public const MODULE_PATH = __DIR__;
-
-    /**
-     * Module version.
-     *
-     * @var string
-     * @since 1.0.0
-     */
-    public const MODULE_VERSION = '1.0.0';
-
-    /**
-     * Module name.
-     *
-     * @var string
-     * @since 1.0.0
-     */
-    public const MODULE_NAME = 'Exchange';
-
-    /**
-     * Module id.
-     *
-     * @var int
-     * @since 1.0.0
-     */
-    public const MODULE_ID = 1007000000;
-
-    /**
-     * Providing.
-     *
-     * @var string[]
-     * @since 1.0.0
-     */
-    protected static $providing = [];
-
-    /**
-     * Dependencies.
-     *
-     * @var string[]
-     * @since 1.0.0
-     */
-    protected static $dependencies = [
-    ];
 
     public function viewExchangeDashboard(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
     {
@@ -210,60 +161,5 @@ final class Controller extends ModuleAbstract implements WebInterface
         $view->addData('lang', $lang);
 
         return $view;
-    }
-
-    /**
-     * Api method to import data
-     *
-     * @param RequestAbstract  $request  Request
-     * @param ResponseAbstract $response Response
-     * @param mixed            $data     Generic data
-     *
-     * @return void
-     *
-     * @api
-     *
-     * @since  1.0.0
-     */
-    public function apiExchangeImport(RequestAbstract $request, ResponseAbstract $response, $data = null) : void
-    {
-        $import  = $this->importDataFromRequest($request);
-        $status  = NotificationLevel::ERROR;
-        $message = 'Import failed.';
-
-        if ($import) {
-            $status  = NotificationLevel::OK;
-            $message = 'Import succeeded.';
-        }
-
-        $response->set($request->getUri()->__toString(), [
-            'status' => status,
-            'title' => 'Exchange',
-            'message' => $message
-        ]);
-    }
-
-    /**
-     * Method to import data based on a request
-     *
-     * @param RequestAbstract $request Request
-     *
-     * @return bool
-     *
-     * @since  1.0.0
-     */
-    private function importDataFromRequest(RequestAbstract $request) : bool
-    {
-        $interfaces = InterfaceManagerMapper::getAll();
-        foreach ($interfaces as $interface) {
-            if ($request->getData('exchange') ?? '' === $interface->getInterfacePath()) {
-                $class    = '\\Modules\\Exchange\\Interfaces\\' . $interface->getInterfacePath() . '\\Importer';
-                $importer = new $class($this->app->dbPool->get());
-
-                return $importer->importFromRequest($request);
-            }
-        }
-
-        return false;
     }
 }
