c7c6f2b8b191f2df1ce533f1593914cee52fb752
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 23:44:00 +0000

    Remove getId() function call from null models

--- tests/Models/NullExchangeSettingTest.php
+++ tests/Models/NullExchangeSettingTest.php
@@ -37,7 +37,7 @@ final class NullExchangeSettingTest extends \PHPUnit\Framework\TestCase
     public function testId() : void
     {
         $null = new NullExchangeSetting(2);
-        self::assertEquals(2, $null->getId());
+        self::assertEquals(2, $null->id);
     }
 
     /**
